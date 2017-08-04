d6d98435922eb4576afbd04ee70a8807b04b2033
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-08-04 18:07:26 +1000

    Route updates and cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// Pages
 Route::get('/', "PagesController@index");
 Route::get('/faq', "PagesController@faq");
 
+// Tickets
 Route::get("/tickets/create", "TicketsController@create");
 Route::get("/tickets", "TicketsController@index");
 Route::post("/tickets", "TicketsController@store");
 Route::get("/tickets/search", "TicketsController@search");
 Route::get("/tickets/{id}", "TicketsController@details");
 
+// Comments
+Route::post("/tickets/{id}/comments", "CommentsController@store");
+
+// User
 Route::get("/staff", "UsersController@staff");
 Route::post("/staff", "UsersController@signin");
-
 Route::get("/logout", "UsersController@logout");
 
