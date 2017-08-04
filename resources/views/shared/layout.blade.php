5a05fbc92b697482573aa05d5b185db26063d265
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-08-04 18:08:01 +1000

    Used entry headers

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -2,38 +2,37 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield("title", "ITS Ticketing System")</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="/css/style.css" />
         @yield("head-scripts")
     </head>
     <body>
         <div class="site-wrapper">
+
             <div class="site-header">
                 @include("shared.site-header")
             </div>
+
             <div class="site-main">
+
+                @yield("entry-header")
+
                 <div class="container">
-                    <div class="clearfix margin sidebar-left entry">
-                    	<div class="fixed-sidebar">
-                    	    <div class="site-sidebar">
-                    	        @include("shared.site-sidebar")
-                    	    </div>
-                    	</div>
-                    	<div class="responsive-content">
-                    		<div class="inner">
-                    		    <div class="site-content">
-                    		        @yield("site-content")
-                    		    </div>
-                    		</div>
-                    	</div>
+                    <div class="site-content">
+                        @yield("site-content")
                     </div>
                 </div>
             </div>
+
             <div class="site-footer">
                 @include("shared.site-footer")
             </div>
+
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
